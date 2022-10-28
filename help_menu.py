3776500f4a9b7e0cd1f033516205e99ed072bfed
Liviu Deleu <liviu.deleu.2002@gmail.com>
2022-10-28 12:17:38 +0300

    help

---
+++ help_menu.py
@@ -0,0 +1,20 @@
+# import statements
+from tkinter import *
+from tkinter.messagebox import *
+
+class Help():
+    def about(root):
+        showinfo(title="About", message="Hello, this is a text editor made by Insidious using Python")
+
+def main(root, text, menubar):
+
+    help = Help()
+
+    helpMenu = Menu(menubar)
+    helpMenu.add_command(label="About", command=help.about)
+    menubar.add_cascade(label="Help", menu=helpMenu)
+
+    root.config(menu=menubar)
+
+if __name__ == "__main":
+    print("Please run 'main.py'")
